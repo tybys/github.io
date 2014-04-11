5fbc81802a66b34d8981b97bc61d7d2d90b6d02e
tybys <gromhs@gmail.com>
2014-04-11 23:13:31 +0400

    argh

--- jworks/fear/thermo/js/helpers.js
+++ jworks/fear/thermo/js/helpers.js
@@ -3,10 +3,18 @@
     $(function ()
     {
         // global parameters for window
-        var bwidth = $(window).outerWidth,
-            bheight = $(window).outerHeight;
+        var bwidth = $(window).outerWidth(),
+            bheight = $(window).outerHeight(),
+            bbSum = (' width ' + bwidth + ' height ' + bheight);
 
-        $('body').append(bwidth, bheight);
+        var bodyCalc = function ()
+        {
+            $('body').append('<div style="position: fixed; top: 15%; left: 5%;">'+ bbSum +'</div>');
+        }();
 
+        $(window).resize(function ()
+        {
+            bodyCalc();
+        });
     });
 })(jQuery);
