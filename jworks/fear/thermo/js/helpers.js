336c9ee7130bed75582b77e99adca493fb83ed50
tybys <gromhs@gmail.com>
2014-04-12 11:22:03 +0400

    thermo item.html fix

--- jworks/fear/thermo/js/helpers.js
+++ jworks/fear/thermo/js/helpers.js
@@ -2,32 +2,35 @@
 {
     $(function ()
     {
-        // global parameters for window
-        var bwidth = $(window).outerWidth(),
-            bheight = $(window).outerHeight(),
-            bbSum = (' width ' + bwidth + ' height ' + bheight);
 
-        var uwidth = $('.unit-centered').eq(0).outerWidth();
+        $('.units-row a').on('click', function (e)
+        {
+            e.preventDefault();
+        });
 
         var bodyCalc = function ()
         {
+            var bwidth = $(window).outerWidth(),
+                bheight = $(window).outerHeight(),
+                bbSum = (' width ' + bwidth + ' height ' + bheight);
+
+            var uwidth = $('.unit-centered').eq(0).outerWidth();
+
             $('body').append('<div id="bodycalc" style="position: fixed; top: 15%; left: 5%;">'+ 'content width ' + uwidth +'</div>');
         }();
 
-        $(window).resize(function ()
-        {
-//            $('#bodycalc').html(bbSum)
-        });
-
         // video link-panel
-        var videoPanel = $('.video a'),
-            videoPanelWidth = videoPanel.parent().outerWidth(),
-            videoPanelHeight = videoPanel.parent().outerHeight();
+        var videoLink = function ()
+        {
+            var videoPanel = $('.video a'),
+                videoPanelWidth = videoPanel.parent().outerWidth(),
+                videoPanelHeight = videoPanel.parent().outerHeight();
 
-        videoPanel.css
-        ({
-            left: videoPanelWidth / 4 - 18+'px',
-            top: videoPanelHeight / 2 - 88+'px'
-        });
+            videoPanel.css
+            ({
+                left: videoPanelWidth / 4 - 18+'px',
+                top: videoPanelHeight / 2 - 88+'px'
+            });
+        }();
     });
 })(jQuery);
