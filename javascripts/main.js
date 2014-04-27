d74959308eee1b198315a2af2433e7a0adfa3e47
tybys <gromhs@gmail.com>
2014-04-27 08:29:25 +0400

    LEFORM clean bootstrap folder

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,11 +3,20 @@
     $(function ()
     {
         $.ajax({
-            url: ''
-        })
-        .done(function ()
+            //url: '/works/fear/leform/leform.html',
+            url: 'works/fear/leform/leform.html',
+            dataType: 'jsonp'
+        }).success(function (data)
         {
-
+            console.log(data)
+        })
+        .error(function (data)
+        {
+            console.log(data)
+        })
+        .done(function (data)
+        {
+            console.log(data)
         });
     });
 })(jQuery);
