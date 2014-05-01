2407b991ad0ca49ca15eaa8f929274c3981b924e
tybys <gromhs@gmail.com>
2014-05-01 07:57:45 +0400

    leform main page

--- works/leform/js/helper.js
+++ works/leform/js/helper.js
@@ -3,5 +3,10 @@
     $(function ()
     {
 
+        $('body').append('<span id="toggle">toggle</span>')
+        $('#toggle').click(function ()
+        {
+            $('#grid').fadeToggle();
+        })
     });
 })(jQuery);
