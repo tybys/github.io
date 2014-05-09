bd31d04180cdfac6756a4d67003da4f915940d4d
tybys <gromhs@gmail.com>
2014-05-10 00:48:28 +0400

    leform

--- works/leform/js/helper.js
+++ works/leform/js/helper.js
@@ -29,7 +29,8 @@
             "video-post",
             "strange-article",
             "recruit",
-            "index-mobile-details");
+            "index-mobile-details",
+            "controls");
         for (var i = 0; i < menu.length; i++)
         {
             var m = $('<a/>', {'href' : menu[i] + '.html'}).text(menu[i]);
@@ -42,17 +43,8 @@
         $('.row .container').append("<div id='grid' style='display: none' />");
 
 
-        $('.shapeshift-container')
-            .shapeshift({
-                enableDrag: false,
-                enableCrossDrop: false,
-                minColumns: 3,
-                align: "left",
-                gutterX: 0,
-                gutterY: 0,
-                paddingX: 0,
-                paddingY: 0
-            });
+        // mosaic block wall items-wall.html
+
 
         $('body').append('<span id="toggle">toggle</span>')
         $('#toggle').click(function ()
@@ -60,43 +52,17 @@
             $('#grid').fadeToggle();
         });
 
-        // second menu
-        var window_width = $(window).outerWidth();
-        var window_height = $(window).outerHeight();
-        var  sm = function ()
-        {
-            var second_menu = $('#second_menu');
-
-            second_menu.css({
-                position: 'absolute',
-                top: window_height / 4
-            });
-        }();
 
         // img resizer, bad idea, wait for fear
         var ir = function ()
         {
-            var img = $('.index');
+        //    var img = $('.index');
 
         }();
 
-        // short_shit_code for backgrounds
-        var img_arr = new Array('first','second','third','four');
-        var br = function ()
-        {
-            setInterval(function ()
-            {
-                $('.index').css({
-//                    backgroundImage:
-                })
-            }, 1000);
-        }
 
-        var cap_height = $('.top').outerHeight();
-        var menu_height = $('.menu').outerHeight();
-        var sum = window_height - (cap_height + menu_height)
 
-        $('.index').css('height', sum)
+
 
         $('.navbar-toggle').on('click', function ()
         {
@@ -104,6 +70,32 @@
             $('#top_nav').slideToggle();
         });
 
+        ////////////////
+        // second menu
+        var window_width = $(window).outerWidth();
+        var window_height = $(window).outerHeight();
+        var  sm = function ()
+        {
+            var second_menu = $('#second_menu');
+            second_menu.css({
+                position: 'absolute',
+                top: window_height / 4
+            });
+        }();
+
+        // index backround sizer
+        var cap_height = $('.top').outerHeight();
+        var menu_height = $('.menu').outerHeight();
+        var sum = window_height - (cap_height + menu_height)
+        $('.index').css('height', sum)
+
+        $('.share span').hover(function ()
+        {
+            $(this).prepend('<span id="background" />');
+        }, function ()
+        {
+            $('#background').remove();
+        });
 
     });
 })(jQuery);
