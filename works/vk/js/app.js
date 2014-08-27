c72d69dcd609cab4ad9217e07b1c63b91ad27033
kirill <gromhs@gmail.com>
2014-08-27 20:27:55 +0400

    fix
    qwe

--- works/vk/js/app.js
+++ works/vk/js/app.js
@@ -4,5 +4,31 @@
        setTimeout(function () {
            //$(".water").addClass("start");
        }, 3000);
+
+        $('#fileUpload').change(function () {
+            var filepath = this.value;
+            m = filepath.match(/([^\/\\]+)$/),
+                filename = m[1];
+
+            //filename.length !== 0 ? "" : "";
+
+            if (filename.length !== 0) {
+                $("#fileWrap").find("i").attr("class", "icon-trash");
+                $('#filename span').toggleClass("ass")
+            }
+
+            $('#filename span').html(filename);
+            //console.log($('#fileUpload').val());
+
+        });
+
+        $("#cleaner").click(function () {
+            if (!$(this).hasClass("icon-paperclip")) {
+                $('#filename span').html(window.messages.attachFile);
+                $("#fileWrap").find("i").attr("class", "icon-paperclip");
+                $('#fileUpload').val("");
+                //console.log($('#fileUpload').val());
+            }
+        });
     });
 })(jQuery);
