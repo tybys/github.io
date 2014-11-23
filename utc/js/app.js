3184b16cdcfd67b56dd2097d9c9cebfa7025126f
kirill <gromhs@gmail.com>
2014-11-23 20:15:35 +0300

    utc, all html are done

---
+++ utc/js/app.js
@@ -0,0 +1,23 @@
+$(function ()
+{
+    temp_menu();
+});
+
+var temp_menu = function ()
+{
+    var rootMenu = $('<div id="sub" />').appendTo('body');
+    var menu = [
+        ["index", "главная"],
+        ["category", "категории"],
+        ["item-card", "карточка товара"]
+    ]
+    for (var i = 0; i < menu.length; i++)
+    {
+        var m = $('<a/>', {'href' : menu[i][0] + '.html'}).text(menu[i][1]);
+        $('#sub').append(m);
+    }
+
+    rootMenu.css({position: 'fixed',top: '1%',right: '0.3%',zIndex: '999'});
+    rootMenu.find('a').css({display: 'block',padding: '3px',color: 'white', background: 'black', margin: '0 1px', opacity: .5});
+    //$('.row').append("<div id='grid' />");
+}
