d81b230e225ad4b4f3979688078cf94662190ad9
kirill <gromhs@gmail.com>
2014-12-01 08:42:24 +0300

    chrome_ext

---
+++ js/chrome_font_ext/popup.js
@@ -0,0 +1,49 @@
+    //function handleContextMenu(event) {
+    //
+    //    var t           = document.defaultView.getComputedStyle(event.target, '');
+    //    var color       = t.getPropertyCSSValue('color');
+    //    var r           = ('00' + parseInt(color.getRGBColorValue().red.cssText).toString(16)).slice(-2);
+    //    var g           = ('00' + parseInt(color.getRGBColorValue().green.cssText).toString(16)).slice(-2);
+    //    var b           = ('00' + parseInt(color.getRGBColorValue().blue.cssText).toString(16)).slice(-2);
+    //    var font_family = t.getPropertyCSSValue('font-family').cssText;
+    //    var font_size   = t.getPropertyCSSValue('font-size').cssText;
+    //    var font_weight = t.getPropertyCSSValue('font-weight').cssText;
+    //    var line_height = t.getPropertyCSSValue('line-height').cssText;
+    //
+    //    chrome.self.tab.setContextMenuEventUserInfo(event,
+    //        {
+    //            'Family': font_family,
+    //            'Size': (font_size + ' / ' + line_height),
+    //            'Weight': font_weight,
+    //            'Color': ('#' + r + g + b).toUpperCase()
+    //        }
+    //    );
+    //
+    //};
+    //
+    //document.addEventListener("contextmenu", handleContextMenu, false);
+
+    function test (event)
+    {
+        var t           = document.defaultView.getComputedStyle(event.target, '');
+        var color       = t.getPropertyCSSValue('color');
+        var r           = ('00' + parseInt(color.getRGBColorValue().red.cssText).toString(16)).slice(-2);
+        var g           = ('00' + parseInt(color.getRGBColorValue().green.cssText).toString(16)).slice(-2);
+        var b           = ('00' + parseInt(color.getRGBColorValue().blue.cssText).toString(16)).slice(-2);
+        var font_family = t.getPropertyCSSValue('font-family').cssText;
+        var font_size   = t.getPropertyCSSValue('font-size').cssText;
+        var font_weight = t.getPropertyCSSValue('font-weight').cssText;
+        var line_height = t.getPropertyCSSValue('line-height').cssText;
+
+        chrome.contextMenus.create(event,
+            {
+                'Family': font_family,
+                'Size': (font_size + ' / ' + line_height),
+                'Weight': font_weight,
+                'Color': ('#' + r + g + b).toUpperCase()
+            }
+        );
+
+    };
+
+    document.addEventListener("contextMenus", test, false)
