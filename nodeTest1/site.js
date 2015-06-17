d97ff7ba9dbeffaa2b1bbb236544c198da1f901a
kirill <gromhs@gmail.com>
2015-06-17 23:15:59 +0300

    node test

--- nodeTest1/site.js
+++ nodeTest1/site.js
@@ -1,13 +1,29 @@
 var path = require('path');
 var express = require('express');
-
+var app = express();
 
 var staticSiteOptions = {
     portnum: 80,
     maxAge: 1000 * 60 * 15
 };
 
-express().use(express.static(
-    path.join(__dirname, 'static'),
-    staticSiteOptions
-)).listen(staticSiteOptions.portnum);
+
+    app.set('view', __dirname + 'app/view');
+    app.set('view_engine', 'jade');
+    app.use(app.mountpath);
+
+
+app.get('/', function (req, res)
+{
+    res.render('index.jade', {
+        title: 'my index jade file'
+    });
+});
+
+app.listen(80);
+
+//express().use(express.static(
+//    path.join(__dirname, 'static'),
+//    staticSiteOptions
+//)).listen(staticSiteOptions.portnum);
+
