6e40b2126a2d9cc70af7bd95157de34642e0ee19
kirill <gromhs@gmail.com>
2015-06-20 01:03:13 +0300

    ж

--- nodeTest1/site.js
+++ nodeTest1/site.js
@@ -1,3 +1,5 @@
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
 var path = require('path');
 var express = require('express');
 var app = express();
@@ -9,6 +11,8 @@ var staticSiteOptions = {
 
 app.set('view_engine', 'jade');
 app.set('views', __dirname + '/views');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.get('/', function (req, res)
 {
@@ -28,35 +32,50 @@ app.get('/create', function (req, res)
 {
     res.render('create.jade', {
 
-    })
+    });
 });
 
-app.post('/report', function (req, res)
+// form handling
+app.post('/create', function(req, res)
 {
-    console.log(1)
+    var lkugraph = req.body.LkuGraph;
+    var lkfgraph = req.body.LkfGraph;
+    var RbGraph = req.body.RbGraph;
+    var RestGraph = req.body.RestGraph;
+    var Rb_Payments = req.body.Rb_Payments;
+    var Showcase = req.body.Showcase;
+    var lku = req.body.lku;
+    var lkf = req.body.lkf;
+    var rb = req.body.rb;
+    var sc = req.body.sc;
+    var pp = req.body.pp;
+    var rest = req.body.rest;
+
+    //res.send(lku);
 });
 
-var mysql      = require('mysql');
 var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : 'tabasov.dunichev.rysin.kfrhfvf'
+    password : '',
+    database: 'express'
 });
 
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-    if (err) throw err;
-    console.log('The solution is: ', rows[0].solution);
-});
-
-connection.end();
 connection.connect();
+connection.query('SELECT * FROM test', function (err, rows, fields)
+{
+    if (err) throw err;
+    console.log(rows)
+});
+
+//connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+//    if (err) throw err;
+//    console.log('The solution is: ', rows[0].solution);
+//});
+
+//connection.end();
+//connection.connect();
 
 
-app.listen(8080);
-
-//express().use(express.static(
-//    path.join(__dirname, 'static'),
-//    staticSiteOptions
-//)).listen(staticSiteOptions.portnum);
-
+app.listen(82);
+app.use(bodyParser());
